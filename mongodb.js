c202e4add6ae58798a7cf39157ff4043875fe11f
Jorden Hilton <jorden.hilton@gmail.com>
2020-09-03 20:20:45 -0500

    Updating docs

--- mongodb.js
+++ mongodb.js
@@ -9,10 +9,6 @@ const { MongoClient, ObjectID } = require("mongodb");
 const connectionURL = "mongodb://127.0.0.1:27017 ";
 const databaseName = "task-manager";
 
-const id = new ObjectID();
-console.log(id.id.length);
-console.log(id.toHexString().length);
-
 MongoClient.connect(
   connectionURL,
   { useNewUrlParser: true },
@@ -23,61 +19,38 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    // db.collection("users").insertOne(
-    //   {
-    //     name: "Vikram",
-    //     age: 26,
-    //   },
-    //   (error, result) => {
-    //     if (error) {
-    //       return console.log("Unable to insert user");
+    // db.collection("users")
+    //   .updateOne(
+    //     {
+    //       _id: new ObjectID("5f51661fdf91386eea879277"),
+    //     },
+    //     {
+    //       $inc: {
+    //         age: 1,
+    //       },
     //     }
+    //   )
+    //   .then(() => {
+    //     console.log(result);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
 
-    //     console.log(result.ops);
-    //   }
-    // );
-
-    // db.collection("users").insertMany(
-    //   [
-    //     {
-    //       name: "Jen",
-    //       age: 28,
-    //     },
-    //     {
-    //       name: "Gunther",
-    //       age: 28,
-    //     },
-    //   ],
-    //   (error, result) => {
-    //     if (error) {
-    //       return console.log("Unable to insert documents!");
-    //     }
-
-    //     console.log(result.ops);
-    //   }
-    // );
-
-    // db.collection("tasks").insertMany(
-    //   [
-    //     {
-    //       description: "Clean the house",
-    //       completed: true,
-    //     },
-    //     {
-    //       description: "Renew inspection",
-    //       complete: false,
-    //     },
-    //     {
-    //       description: "Pot plants",
-    //       completed: false,
-    //     },
-    //   ],
-    //   (error, result) => {
-    //     if (error) {
-    //       return console.log("Unable to insert tasks!");
-    //     }
-    //     console.log(result.ops);
-    //   }
-    // );
+    db.collection("tasks")
+      .updateMany(
+        {
+          completed: false,
+        },
+        {
+          $set: { completed: true },
+        }
+      )
+      .then((result) => {
+        console.log(result.modifiedCount);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 );
